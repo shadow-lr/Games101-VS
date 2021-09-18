b44112c6cd29e7dc66be51d07e8d4b996c0ca5da
shadow-lr <shadow_lr@163.com>
2021-09-18 14:55:31 +0800

    try to add Microfacet and sphere(80%)

--- Homework7/Assignment7/Renderer.cpp
+++ Homework7/Assignment7/Renderer.cpp
@@ -24,7 +24,7 @@ void Renderer::Render(const Scene& scene)
     //int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 600;
+    int spp = 800;
     std::cout << "SPP: " << spp << "\n";
 
     int finish_num = 0;
